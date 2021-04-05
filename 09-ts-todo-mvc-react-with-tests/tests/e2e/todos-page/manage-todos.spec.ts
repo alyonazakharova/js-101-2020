7a194305547bf5714dd47e813da1b5d5046e9190
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2021-04-05 11:11:00 +0300

    added intercept for delete and non empty list fixture

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
@@ -1,6 +1,6 @@
 import { todosEndpoint, getUserEndpoint, pageUrl } from '../../../src/consts/urls';
 
-function authorize() {
+function authorize(fixture: string) {
   cy.intercept(
     'GET',
     getUserEndpoint,
@@ -10,13 +10,14 @@ function authorize() {
   cy.intercept(
     'GET',
     todosEndpoint,
-    { fixture: 'empty-todos-list.response.json' }
+    { fixture: fixture }
   ).as('initialTodos');
 
   cy.visit(pageUrl)
     .wait('@authorize')
     .wait('@initialTodos');
 }
+
 
 describe('Manage Todos', () => {
   context('Creation', () => {
@@ -40,7 +41,8 @@ describe('Manage Todos', () => {
 
       const itemText = Math.random().toString() + '_' + Date.now();
 
-      authorize();
+      authorize('empty-todos-list.response.json');
+
       cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
       cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
       cy.get('[data-test-id=create-new-todo-form]').submit();
@@ -48,7 +50,6 @@ describe('Manage Todos', () => {
       cy.wait('@createTodo');
 
       cy.get('[data-test-id=todos-list]').should('be.visible');
-
       cy.get('[data-test-id=todo-item]').should('be.visible');
       cy.get('[data-test-id=todo-item__checked-checkbox]').should('not.be.checked');
       cy.get('[data-test-id=todo-item__text-input]').should('have.value', itemText);
@@ -58,31 +59,29 @@ describe('Manage Todos', () => {
 
   context('Deletion', () => {
     it('Delete single todo', () => {
-      cy.fixture('todo-item.response.json').then((todoItemResponse)  => {
+      cy.fixture('todos-list.response.json').then((todoItem) => {
+        let id = todoItem[0]['id'];
         cy.intercept(
-            'POST',
+            'DELETE',
             todosEndpoint,
             req => {
-              const { body } = req;
-              req.reply({
-                statusCode: 200,
-                body: JSON.stringify({
-                  ...todoItemResponse,
-                  text: body.text
-                })
-              });
+              const {body} = req;
+              expect(body.id).to.equal(id);
+              req.reply();
             }
-        ).as('createTodo');
+        ).as('deleteTodo');
       });
 
-      authorize();
+      authorize('todos-list.response.json');
 
-      const itemText = Math.random().toString() + '_' + Date.now();
-      cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
-      cy.get('[data-test-id=create-new-todo-form]').submit();
-      cy.wait('@createTodo');
+      cy.get('[data-test-id=todos-list]').should('be.visible');
+      cy.get('[data-test-id=todo-item]').should('be.visible');
+      cy.get('[data-test-id=task-counter]').should('have.text', "1 item left");
 
       cy.get('[data-test-id=todo-item__remove-action]').click({force: true})
+
+      cy.wait('@deleteTodo');
+
       cy.get('[data-test-id=todo-item]').should('not.exist');
       cy.get('[data-test-id=todos-list]').should('be.empty');
       cy.get('[data-test-id=task-counter]').should('have.text', "0 items left");
